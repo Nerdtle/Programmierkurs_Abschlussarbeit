ef2efbf385bc77c6de0a5b7be2e3ca5ea86cf9db
Jaschimi <jascha-schulz@web.de>
2018-09-25 00:45:10 +0200

    Implemented constructors
    For classes Gerade and Ebene. Testing will commence soon.

--- Themengebiet_2_Geometrie_Ebenen_und_Geraden.py
+++ Themengebiet_2_Geometrie_Ebenen_und_Geraden.py
@@ -3,13 +3,13 @@
 #betrachtet werden. Hierzu schreibe man zuerst die zwei Klassen Gerade und Ebene mit den nicht-öffentlichen
 #Attributen x_0,vec_1,vec_2 im Falle von Ebenen. x_0,vec_1 im Falle von Geraden.
 #Hierbei sollen die Ebenen bzw. Geraden in Parameterform dargestellt sein. Man schreibe Konstruktoren
-#um die Objekte aus vorgegebenen x_0,vec_1,vec_2 bzw. x_0,vec_1 zu erzeugen.
-# Die Parameter sollen hier Listen sein. Zusätzlich soll für beide Klassen eine Methode move(vec)
-# geschrieben werden, die die Gerade/Ebene um vec verschiebt und eine Methode ison(x), die ausgibt,
-#ob der Punkt x(Liste/Tupel) auf der Gerade/Ebene liegt. Für alle in diesem Themengebiet geschriebenen
-#Klassen stelle man vernünftige (passende) Konstruktoren, und set, get-Methoden und geeignete Ausgaben,
-#__str__ und __repr__, zur Verfügung.
-# Man überprüfe stets die Funkltionalität der implementierten Methoden.
+#um die Objekte aus vorgegebenen x_0,vec_1,vec_2 bzw. x_0,vec_1 zu erzeugen. Die Parameter sollen hier
+#Listen sein.
+#Zusätzlich soll für beide Klassen eine Methode move(vec)geschrieben werden, die die Gerade/Ebene um vec
+#verschiebt und eine Methode ison(x), die ausgibt, ob der Punkt x(Liste/Tupel) auf der Gerade/Ebene liegt.
+#Für alle in diesem Themengebiet geschriebenen Klassen stelle man vernünftige (passende) Konstruktoren,
+#und set, get-Methoden und geeignete Ausgaben, __str__ und __repr__, zur Verfügung.
+#Man überprüfe stets die Funkltionalität der implementierten Methoden.
 
 #Projekt I: Man schreibe eine Methode getnormal(), die einen Vektor(Liste) zurückliefert, der senkrecht
 #auf der Ebene steht. Anschließend schreibe man eine weitere Klasse EbeneHess, die die nichtöffentlichen
@@ -21,11 +21,31 @@
 
 class Ebene:
     def __init__(self, x0, vector1, vector2):
-        self._x0 = x0
-        self._vector1 = vector1
-        self._vector2 = vector2
+        if(type(x0)==list and type(vector1)==list and type(vector2)==list):
+            if(len(x0)==3 and len(vector1)==3 and len(vector2)==3):
+                self._x0 = x0
+                self._vector1 = vector1
+                self._vector2 = vector2
+            else:
+                raise IndexError("x0, vector1, and vector2 need to be of length 3! x0 was of length '" +
+            "{}', vector1 was of type '{}', ".format(len(x0), len(vector1)) +
+            "and vector2 was of type '{}'.".format(len(vector2)))
+
+        else:
+            raise TypeError("x0, vector1, and vector2 need to be lists! x0 was of type '" +
+            "{}', vector1 was of type '{}', ".format(type(x0), type(vector1)) +
+            "and vector2 was of type '{}'.".format(type(vector2)))
 
 class Gerade:
     def __init__(self, x0, vector1):
-        self._x0 = x0
-        self._vector1 = vector1
+        if(type(x0)==list and type(vector1)==list):
+            if(len(x0)==3 and len(vector1)==3):
+                self._x0 = x0
+                self._vector1 = vector1
+            else:
+                raise IndexError("x0 and vector1 need to be of length 3! x0 was of length '" +
+            "{}' and vector1 was of length '{}'.".format(len(x0), len(vector1)))
+
+        else:
+            raise TypeError("x0 and vector1 need to be lists! x0 was of type '"
+            + "{}' and vector1 was of type '{}'.".format(type(x0), type(vector1)))
